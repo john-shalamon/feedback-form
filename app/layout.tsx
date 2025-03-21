59c153f642d1ba1edc8006d236bd22347d62236e
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-03-21 21:43:08 +0530

    update metadata in layout.tsx for feedback app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Feedback',
+  description: 'Created with John Shalamon',
+  generator: 'John Shalamon',
 }
 
 export default function RootLayout({
